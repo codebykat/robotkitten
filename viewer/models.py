ba5dbceb45997d40a5fd492bd69739126957563b
kat <kat@codebykat.com>
2010-04-26 16:44:30 +0000

    cleaned up

--- viewer/models.py
+++ viewer/models.py
@@ -1,11 +1,1 @@
 from django.db import models
-
-#class Comic(models.Model):
-#  def __unicode__(self): return "%03d: %s" % (self.id, self.title)
-#  id = models.IntegerField(primary_key=True)
-#  title = models.CharField(max_length=50)
-  #comic = models.ImageField()
-#  url = models.URLField(verify_exists=False)
-#  alt = models.CharField(max_length=128)
-#  date = models.DateField()
-#  news = models.TextField()
